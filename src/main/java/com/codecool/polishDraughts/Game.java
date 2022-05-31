4452e1314791902798130d00613a322432652b1b
Mark Palinkas <p.mark9807@gmail.com>
2022-05-31 11:31:58 +0200

    feat: check player and pawn color

--- src/main/java/com/codecool/polishDraughts/Game.java
+++ src/main/java/com/codecool/polishDraughts/Game.java
@@ -1,16 +1,40 @@
 package com.codecool.polishDraughts;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Game {
-    public static void start() {
+    private Board gameBoard = new Board(10);
+    private Player playerOne = new Player("black");
+    private Player playerTwo = new Player("white");
 
+    public void start() {
+        playRound(playerOne);
+        playRound(playerTwo);
+        this.gameBoard.toString();
     }
 
-    public static void playRound() {
+    public void playRound(Player player) {
+        this.gameBoard.toString();
+        int rowFrom = player.getUserInput("Row from move: ");
+        int columnFrom = player.getUserInput("Column from move: ");
 
+        int rowTo = player.getUserInput("Row to move: ");
+        int columnTo = player.getUserInput("Column to move: ");
+
+         System.out.println(tryToMakeMove(player, rowFrom, columnFrom, rowTo, columnTo));
+
+        //this.gameBoard.removePawn(row, column);
     }
 
-    public static boolean tryToMakeMove() {
-        return true;
+    public boolean tryToMakeMove(Player player, int rowFrom, int columnFrom, int rowTo, int columnTo) {
+        return checkPlayerAndPawnColor(player, rowFrom, rowTo);
+    }
+
+    public boolean checkPlayerAndPawnColor(Player player, int row, int column) {
+        Pawn[][] board = this.gameBoard.getBoard();
+        String playerColor = player.getColor();
+        String pawnColor = board[row][column].getColor();
+        return playerColor.equals(pawnColor);
     }
 
     public static boolean checkForWinner() {
