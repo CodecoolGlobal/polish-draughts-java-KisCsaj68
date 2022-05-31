e0c1520df853679fc8ae8cf0ca52583ce4ce2196
Mark Palinkas <p.mark9807@gmail.com>
2022-05-31 09:32:27 +0200

    feat: implement Player

---
+++ src/main/java/com/codecool/polishDraughts/Player.java
@@ -0,0 +1,20 @@
+package com.codecool.polishDraughts;
+
+public class Player {
+    private Color color;
+
+
+    public Player(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return this.color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+
+}
