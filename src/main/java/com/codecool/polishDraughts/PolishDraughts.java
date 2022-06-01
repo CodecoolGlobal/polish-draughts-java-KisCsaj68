e0fec997e170b709693ede5a69efd82c08702f71
Zsolnai Viki <zsolnai.viki@gmail.com>
2022-06-01 16:33:35 +0200

    feat: implement board size by user input

--- src/main/java/com/codecool/polishDraughts/PolishDraughts.java
+++ src/main/java/com/codecool/polishDraughts/PolishDraughts.java
@@ -1,11 +1,32 @@
 package com.codecool.polishDraughts;
 
+import java.util.Scanner;
+
 public class PolishDraughts {
 
     public static void main (String[] args) {
-
-        Game newGame = new Game(10);
+        System.out.println("Please define the board size(Between 10 and 20):");
+        int size = getBoardSize();
+        while (!isValidBoardSize(size)) {
+            System.out.println("Invalid board size! Try again!");
+            getBoardSize();
+        }
+        Game newGame = new Game(size);
         newGame.start();
-        //gameBoard.toString();
+    }
+
+    private static boolean isValidBoardSize(int n) {
+        return (n >= 10 && n <=20);
+    }
+
+    private static int getBoardSize() {
+        Scanner input = new Scanner(System.in);
+        try {
+            int size = input.nextInt();
+            return size;
+        }catch (Exception e) {
+            return -1;
+        }
+
     }
 }
