cad16a2d400bb74d554e86b890f8225c03cc24d8
Viki <34596151+KisCsaj68@users.noreply.github.com>
2022-06-01 13:20:13 +0200

    Feature/is crowned (#9)
    * feat: implement crowned a pown
    
    * fix: crowned symbol changed

--- src/main/java/com/codecool/polishDraughts/Pawn.java
+++ src/main/java/com/codecool/polishDraughts/Pawn.java
@@ -1,22 +1,33 @@
 package com.codecool.polishDraughts;
 
 public class Pawn {
-    Color color;
-    Coordinate coordinate;
+     private Color color;
+    private Coordinate coordinate;
+    private boolean isCrowned;
 
-    public Pawn(String color, int x, int y) {
+    public Pawn(String color, int x, int y, boolean isCrowned) {
         this.color = new Color(color);
         this.coordinate = new Coordinate(x, y);
+        this.isCrowned = isCrowned;
 
     }
 
+    public void setCrowned(boolean crowned) {
+        isCrowned = crowned;
+    }
+
+
+
     public boolean isCrowned() {
-        return false;
+        return this.isCrowned;
     }
 
     @Override
     public String toString() {
-        return color.getColor().equals("white") ? "☻" : "☺";
+        if(!isCrowned) {
+            return color.getColor().equals("white") ? "☻" : "☺";
+        }
+        return color.getColor().equals("white") ? "■" : "⌂";
 
     }
 
